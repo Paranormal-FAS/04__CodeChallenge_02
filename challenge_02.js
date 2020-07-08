24d700b1fc220b4a7195ad1eb5c1febf2061c88b
Peter Schmidt <schmidt.multimedia@gmail.com>
2020-07-08 12:14:30 +0200

    Add files via upload

---
+++ challenge_02.js
@@ -0,0 +1,18 @@
+/*********   CODE CHALLENGE 02 **********/
+
+/*
+Geben über eine Prompt-Anforderung einen Namen ein
+Geben über eine Prompt-Anforderung ein Alter ein
+
+Geben Sie in Abhängigkeit vom angegebenen Alter 
+folgende Getränkeauswahl in die Konsole aus aus :
+
+Alter 0-5 // Milch
+Alter 6-12 // Saft
+Alter 13-17 // Cola
+18 + // Wein
+
+*/
+
+/* Getränke-Challenge */
+
